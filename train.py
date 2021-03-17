950219ed63126a8f67b0591efe2e6c41d0ab1f84
WangYue <wavewangyue@163.com>
2021-03-17 17:51:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,29 +1,34 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
- 
+# -*- coding: utf-8 -*- 
+
 import logging
 import os.path
 import sys
 import multiprocessing
- 
+import gensim
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
- 
-if __name__ == '__main__':
-    i_file = 'corpus.txt'
-    m_file = 'yelp.model'
-    v_file = 'yelp.vector.bin'
- 
+import jieba
+
+def w2v_train(fin, fout):
     program = os.path.basename(sys.argv[0])
     logger = logging.getLogger(program)
     logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
     logging.root.setLevel(level=logging.INFO)
     logger.info("running %s" % ' '.join(sys.argv))
     
-    model = Word2Vec(LineSentence(i_file), size=200, window=5, min_count=50,
-            workers=multiprocessing.cpu_count())
- 
-    # trim unneeded model memory = use(much) less RAM
-    #model.init_sims(replace=True)
-    model.save(m_file)
-    model.wv.save_word2vec_format(v_file, binary=True)
+    model = Word2Vec(LineSentence(fin), size=100, window=5, min_count=50, workers=multiprocessing.cpu_count())                 
+    model.wv.save_word2vec_format(fout, binary=True)
+        
+
+def trans_bin_to_txt(fin, fout):
+    model=gensim.models.KeyedVectors.load_word2vec_format(fin, binary=True)
+    vocab = model.vocab
+    print("vocab size:", len(vocab))
+    with open(fout, "w") as w:
+        w.write(str(len(vocab)) + " " + str(model.vector_size) + "\n")
+        for word in vocab:
+            w.write(" ".join([word]+[str(num) for num in model[word]]) + "\n")
+
+if __name__ == "__main__":
+    w2v_train("corpus.wiki", "word2vec.wiki.bin")
+    #trans_bin_to_txt("word2vec.wiki.bin", "word2vec.wiki.txt")
